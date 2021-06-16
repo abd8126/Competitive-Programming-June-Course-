70b87eb2020443ced5c142e29448f867b41d8fe6
SHREYA SINGH <50830709+SinghShreya05@users.noreply.github.com>
2021-06-16 18:34:47 +0530

    Add files via upload

---
+++ Shreya/Arrays and Strings/Set Matrix Zeroes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int, greater<int>> row;
+        set<int, greater<int>> col;
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row.insert(i);
+                    col.insert(j);
+                }
+            }   
+        }
+        for (auto it = row.begin(); it != row.end(); ++it) 
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                matrix[*it][j] = 0;
+            }
+        }
+        for (auto it = col.begin(); it != col.end(); ++it) 
+        {
+            for (int i = 0; i < matrix.size(); i++)
+            {
+                matrix[i][*it] = 0;
+            }
+        }
+        
+    }
+};
