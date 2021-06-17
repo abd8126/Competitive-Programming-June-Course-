5fb3104a5803985017dea015424e9abce275f187
Jeevika Wadhwa <56202325+MeeeCoder@users.noreply.github.com>
2021-06-17 22:40:52 +0530

    Update 04_Longest_Substring_without_Repetition.cpp

--- Jeevika Wadhwa/Arrays and Strings/04_Longest_Substring_without_Repetition.cpp
+++ Jeevika Wadhwa/Arrays and Strings/04_Longest_Substring_without_Repetition.cpp
@@ -1,3 +1,14 @@
-Doubt https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
-https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/779/
-https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlength = 0;
+        int index[128] = {0};
+        for(int i=0,j=0;i<s.length();i++){
+            char c = s[i];
+            j = max(j, index[c]);
+            maxlength = max(maxlength, i-j+1);
+            index[c] = i+1;
+        }
+        return maxlength;
+    }
+};
