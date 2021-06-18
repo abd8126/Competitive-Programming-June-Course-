4e132ec2e457491af13a153fe32f5caa07105609
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2021-06-19 00:44:20 +0530

    Update First Unique Character in a String.cpp

--- Aman/Arrays & strings/First Unique Character in a String.cpp
+++ Aman/Arrays & strings/First Unique Character in a String.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int n=s.length();
-        unordered_map<int,int>mp;
+        int dat[26]={0};
         for(int i=0;i<n;i++)
-            mp[s[i]]++;
+            dat[s[i]-'a']++;
         for(int i=0;i<n;i++)
-          if(mp[s[i]]==1)
+          if(dat[s[i]-'a']==1)
             return i;
         return -1;
         
