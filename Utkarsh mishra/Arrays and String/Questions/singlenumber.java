113156f4cd368f2260802de5111a5127d7408285
utkarsh7237 <59720643+utkarsh7237@users.noreply.github.com>
2021-06-18 11:50:19 +0530

    Update singlenumber.java

--- Utkarsh mishra/Arrays and String/Questions/singlenumber.java
+++ Utkarsh mishra/Arrays and String/Questions/singlenumber.java
@@ -3,12 +3,10 @@ class Solution {
     public int singleNumber(int[] nums) {
         Map<Integer,Integer> map=new HashMap<>();
        for(int num:nums){
-            if(map.containsKey(num))
-                map.put(num,map.get(num)+1);
-            else
-                map.put(num,1);}
+          
+                map.put(num,map.getOrDefault(num,0)+1);
+            }
        int v=1;
-
          for(Entry<Integer, Integer> entry: map.entrySet()) {
 
       if(entry.getValue() == v) {
