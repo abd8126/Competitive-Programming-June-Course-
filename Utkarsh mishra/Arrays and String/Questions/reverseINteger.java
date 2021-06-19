9d23bcda836692bd0e9f2ff6c54466b1062fdef2
utkarsh7237 <59720643+utkarsh7237@users.noreply.github.com>
2021-06-19 15:43:21 +0530

    Create reverseINteger.java

---
+++ Utkarsh mishra/Arrays and String/Questions/reverseINteger.java
@@ -0,0 +1,18 @@
+class Solution {
+   
+public int reverse(int x) {
+long rev = 0;
+
+while (x != 0) {
+rev = rev * 10 + x % 10;
+
+if ((x > 0 && rev > Integer.MAX_VALUE )|| (x < 0 && rev < Integer.MIN_VALUE)) {
+return 0;
+}
+
+x /= 10;
+
+}
+return (int) rev;
+}
+	}
