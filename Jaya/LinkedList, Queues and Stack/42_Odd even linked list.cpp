fcdb0ee424fd8cb2d83a02e52514dd8afb5190ff
jaya <rajjaya2907.jr@gmail.com>
2021-06-25 15:27:11 +0530

    Update 42_Odd even linked list.cpp

--- Jaya/LinkedList, Queues and Stack/42_Odd even linked list.cpp
+++ Jaya/LinkedList, Queues and Stack/42_Odd even linked list.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head == nullptr || head->next == nullptr || head->next->next==nullptr) return head;
+        if(head == nullptr || head->next == nullptr) return head;
         ListNode *odd=head;
         ListNode *even=head->next;
         ListNode *evenptr=even;
-        while(odd->next!=nullptr && even->next!=nullptr){
+        while(even!=nullptr && even->next!=nullptr){
             odd->next=even->next;
             odd=odd->next;
             even->next=odd->next;
             even=even->next;
         }
         odd->next=evenptr;
-        if(odd->next==nullptr) even->next=nullptr;
         return head;
     }
 };
