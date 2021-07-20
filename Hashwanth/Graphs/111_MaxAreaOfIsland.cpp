f6c15bf834a7c8fbcd6bee4751c11c220986c088
hashwnath <53908470+hashwnath@users.noreply.github.com>
2021-07-20 16:19:50 +0530

    Create 111_MaxAreaOfIsland.cpp

---
+++ Hashwanth/Graphs/111_MaxAreaOfIsland.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if(grid.size()==0)
+            return 0;
+        int a=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    int ca=dfs(grid,i,j);
+                    a=max(a,ca);
+                }
+            }
+        }
+        return a;
+    }
+    int dfs(vector<vector<int>>& grid, int i, int j)
+    {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0)
+            return 0;
+        grid[i][j]=0;
+        
+        return 1+dfs(grid,i-1,j)+dfs(grid,i+1,j)+dfs(grid,i,j+1)+dfs(grid,i,j-1);
+        
+    }
+};
