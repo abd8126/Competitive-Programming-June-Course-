142d6640bdbec5cc64a906b37ec59c1f85d08cc4
Jeevika Wadhwa <56202325+MeeeCoder@users.noreply.github.com>
2021-07-25 13:48:19 +0530

    Create 138_BT_Level_Order_Traversal_II.cpp

---
+++ Jeevika Wadhwa/Tree/138_BT_Level_Order_Traversal_II.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root == NULL)return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int>level;
+            while(n--){
+                TreeNode* curr = q.front();q.pop();
+                level.push_back(curr->val);
+                if(curr->left)q.push(curr->left);
+                if(curr->right)q.push(curr->right);
+            }
+            ans.push_back(level);
+        }
+        reverse(begin(ans), end(ans));
+        return ans;
+    }
+};
