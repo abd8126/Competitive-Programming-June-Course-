ab1aeadd7bce70a8d306056c1e9e8515a9d62f05
Jeevika Wadhwa <56202325+MeeeCoder@users.noreply.github.com>
2021-07-25 13:43:11 +0530

    Create 137_Average_of_levels_in_BT.cpp

---
+++ Jeevika Wadhwa/Tree/137_Average_of_levels_in_BT.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double>ans;
+        if(root == NULL)return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            double sum = 0;
+            for(int i = 0; i<n; i++){
+                TreeNode*curr = q.front();q.pop();
+                sum += curr->val;
+                if(curr->left)q.push(curr->left);
+                if(curr->right)q.push(curr->right);
+            }
+            sum = (double)sum/n;
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
