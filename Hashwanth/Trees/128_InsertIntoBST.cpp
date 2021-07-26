3c59cd637b4aa27516be5c842d4953084422f901
hashwnath <53908470+hashwnath@users.noreply.github.com>
2021-07-26 18:09:59 +0530

    Create 128_InsertIntoBST.cpp

---
+++ Hashwanth/Trees/128_InsertIntoBST.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL)
+        {
+            TreeNode* temp=new TreeNode(val);
+            return temp;
+           
+        }
+        else if(val<=root->val)
+        {
+            root->left=insertIntoBST(root->left,val);
+            return root;
+        }
+        else
+        { root->right=insertIntoBST(root->right,val);
+         return root;
+        } 
+    }
+};
