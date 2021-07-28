b471460f03af0cb3675febbd358be957ac1eca88
hashwnath <53908470+hashwnath@users.noreply.github.com>
2021-07-28 23:02:53 +0530

    Create 152_PathSum.cpp

---
+++ Hashwanth/Trees/152_PathSum.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+         if(!root)
+            return false;
+        else if(!root->left && !root->right && root->val==targetSum)
+            return true;
+      
+        return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
+    }
+};
